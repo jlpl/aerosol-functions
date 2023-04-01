1ddcc3bc1b83e56401a031917ab48de32fbfdcfb
jlpl <janne.lampilahti@gmail.com>
2023-04-01 10:12:07 +0300

    Add plotting module

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md","r") as fh:
 
 setup(
     name="aerosol-functions",
-    version="0.0.7",
+    version="0.0.8",
     description='Functions to analyze atmospheric aerosol data',
     package_dir={'':'src'},
     packages=['aerosol'],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires='>=3.9',
     install_requires = [
         "pandas >= 1.1.0",
         "numpy >= 1.19.0",
